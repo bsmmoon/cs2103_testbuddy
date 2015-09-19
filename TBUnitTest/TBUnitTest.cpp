5986b807806e1f8988c5b440f9a5e0cdec42eb8d
bsmmoon <bsmmoon@powerme.com.sg>
2015-09-19 15:09:18 +0800

    UnitTest will use assertion to show the actual and expected instead of making a separate text file

--- TBUnitTest/TBUnitTest.cpp
+++ TBUnitTest/TBUnitTest.cpp
@@ -7,6 +7,8 @@
 #include "TextBuddyMain.h"
 #include "TextBuddyLibrary.h"
 
+#include <algorithm>
+
 using namespace std;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -19,25 +21,30 @@ private:
 
 public:
 	void writeVectorToFile(ofstream &file, vector<string> list) {
-		for (int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < (int) list.size(); i++) {
 			file << list.at(i) << " ";
 		}
 		file << "\n";
 	}
 
-	void execUnitTest(vector<vector<string>> input) {
+	string implode(vector<string> vec) {
+		string str = "{ ";
+		for (int i = 0; i < (int) vec.size(); i++) {
+			str += vec.at(i) + ", ";
+		}
+		str += " }";
+		return str;
+	}
+
+	void execUnitTest(vector<string> initial, string command, vector<string> arguments, vector<string> expected) {
 		TextBuddyMain tm(testFile);
-		vector<string> initial = input.at(0);
-		string command = input.at(1).at(0);
-		vector<string> arguments = input.at(2);
-		vector<string> expected = input.at(3);
 
 		vector<string> actual;
 		try {
 			if (arguments.size() == 0) {
 				actual = tm.execCommand(testFile, initial, { command, "NULL" });
 			} else {
-				for (int i = 0; i < arguments.size(); i++) {
+				for (int i = 0; i < (int) arguments.size(); i++) {
 					actual = tm.execCommand(testFile, initial, { command, arguments.at(i) });
 				}
 			}
@@ -45,21 +52,10 @@ public:
 			actual = { message.what() };
 		}
 
-		if (actual == expected) {
-			Assert::IsTrue(true);
-		} else {
-			ofstream file("UnitTestLog.txt", std::ios_base::app);
-			file << "----------\n";
-			writeVectorToFile(file, initial);
-			file << command << "\n";
-			writeVectorToFile(file, arguments);
-			writeVectorToFile(file, actual);
-			writeVectorToFile(file, expected);
-			file << "----------\n";
-			file.close();
+		string expectedStr = implode(expected);
+		string actualStr = implode(actual);
 
-			Assert::IsTrue(false);
-		}
+		Assert::AreEqual(expectedStr, actualStr);
 	}
 
 	TEST_METHOD_INITIALIZE(START) {
@@ -68,35 +64,32 @@ public:
 	TEST_METHOD_CLEANUP(CLEAN) {
 	}
 
-	// TEMPLATE:
-	// tm.execCommand( {initial}, {command}, {arguments}, {expected} );
-
 	TEST_METHOD(addTaskTest) {
-		execUnitTest({ {}, { "add" }, {}, { "'add' command requires argument" } });
-		execUnitTest({ {}, { "add" }, { "5", "4", "3", "2", "1" }, { "5", "4", "3", "2", "1" } });
-		execUnitTest({ { "5", "4", "3", "2", "1" }, { "add" }, { "10", "9", "8", "7", "6" }, { "5", "4", "3", "2", "1", "10", "9", "8", "7", "6" } });
+		execUnitTest( {}, "add", {}, { "'add' command requires argument" } );
+		execUnitTest( {}, "add", { "5", "4", "3", "2", "1" }, { "5", "4", "3", "2", "1" } );
+		execUnitTest( { "5", "4", "3", "2", "1" }, "add", { "10", "9", "8", "7", "6" }, { "5", "4", "3", "2", "1", "10", "9", "8", "7", "6" } );
 	}
 
 	TEST_METHOD(deleteTaskTest) {
-		execUnitTest({ { "1" }, { "delete" }, {}, { "'delete' command requires argument" } });
-		execUnitTest({ { "1" }, { "delete" }, { "17" }, { "No task with index 17" } });
-		execUnitTest({ { "5", "4", "3", "2", "1" }, { "delete" }, { "3", "3" }, { "5", "4", "1" } });
+		execUnitTest( { "1" }, "delete", {}, { "'delete' command requires argument" } );
+		execUnitTest( { "1" }, "delete", { "17" }, { "No task with index 17" } );
+		execUnitTest( { "5", "4", "3", "2", "1" }, "delete", { "3", "3" }, { "5", "4", "1" } );
 	}
 
 	TEST_METHOD(clearTaskTest) {
-		execUnitTest({ { "5", "4", "3", "2", "1" }, { "clear" }, {}, {} });
+		execUnitTest( { "5", "4", "3", "2", "1" }, { "clear" }, {}, {} );
 	}
 
 	TEST_METHOD(searchListTest) {
-		execUnitTest({ { "nice", "nicer", "nasa", "nicest", "nike" }, { "search" }, {}, { "'search' command requires argument" } });
-		execUnitTest({ { "nice", "nicer", "nasa", "nicest", "nike" }, { "search" }, { "nice" }, { "nice", "nicer", "NULL", "nicest", "NULL" } });
-		execUnitTest({ { "nice", "nicer", "nasa", "nicest", "nike" }, { "search" }, { "n" }, { "nice", "nicer", "nasa", "nicest", "nike" } });
-		execUnitTest({ { "nice", "nicer", "nasa", "nicest", "nike" }, { "search" }, { "z" }, { "NULL", "NULL", "NULL", "NULL", "NULL" } });
+		execUnitTest( { "nice", "nicer", "nasa", "nicest", "nike" }, "search", {}, { "'search' command requires argument" } );
+		execUnitTest( { "nice", "nicer", "nasa", "nicest", "nike" }, "search", { "nice" }, { "nice", "nicer", "NULL", "nicest", "NULL" } );
+		execUnitTest( { "nice", "nicer", "nasa", "nicest", "nike" }, "search", { "n" }, { "nice", "nicer", "nasa", "nicest", "nike" } );
+		execUnitTest( { "nice", "nicer", "nasa", "nicest", "nike" }, "search", { "z" }, { "NULL", "NULL", "NULL", "NULL", "NULL" } );
 	}
 
 	TEST_METHOD(sortListTest) {
-		execUnitTest({ { "5", "4", "3", "2", "1" }, { "sort" }, {}, { "1", "2", "3", "4", "5" } });
-		execUnitTest({ { "5", "4", "3", "2", "1", "9", "8", "7", "6" }, { "sort" }, {}, { "1", "2", "3", "4", "5", "6", "7", "8", "9" } });
+		execUnitTest( { "5", "4", "3", "2", "1" }, "sort", {}, { "1", "2", "3", "4", "5" } );
+		execUnitTest( { "5", "4", "3", "2", "1", "9", "8", "7", "6" }, "sort", {}, { "1", "2", "3", "4", "5", "6", "7", "8", "9" } );
 	}
 	};
 }
